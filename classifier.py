edc5908b786a3deec71ee5800d31edf6cfd704e5
Ammon Dodson <ammon@ammondodson.info>
2019-04-24 01:18:45 -0700

    build and test text Classifier

--- classifier.py
+++ classifier.py
@@ -2,6 +2,7 @@
 import pandas
 
 import baseline
+import textClassifier
 
 header = ['userid','age','gender','ope','con','ext','agr','neu']
 
@@ -13,25 +14,48 @@ header = ['userid','age','gender','ope','con','ext','agr','neu']
 #	@param textPath a string with the text directory
 #	@returns a profile table as a DataFrame
 def base(profileTable,liwcTable,relationTable,imagePath,textPath):
-	#results = pandas.DataFrame(profileTable)
+	results = pandas.DataFrame(index=profileTable['userid'])
+	results['age']    = baseline.MEDIAN_AGE
+	results['gender'] = baseline.MEDIAN_GENDER
+	results['ope']    = baseline.MEAN_OPEN
+	results['con']    = baseline.MEAN_CON
+	results['ext']    = baseline.MEAN_EXT
+	results['agr']    = baseline.MEAN_AGR
+	results['neu']    = baseline.MEAN_NEU
 	
-	results = profileTable.apply(lambda x: 
-		[x['userid'],
-		baseline.MEDIAN_AGE, 
-		baseline.MEDIAN_GENDER,
-		baseline.MEAN_OPEN, 
-		baseline.MEAN_CON, 
-		baseline.MEAN_EXT, 
-		baseline.MEAN_AGR, 
-		baseline.MEAN_NEU],result_type='broadcast', axis=1)
+	return results
+
+
+##	The baseline classifier
+#
+#	Results of classifiers should be indexed by userid so that they can be
+#	combined
+#
+#	@param profileTable the profile table as a DataFrame
+#	@param liwcTable the LIWC table as a DataFrame
+#	@param relationTable the relation table as a DataFrame
+#	@param imagePath a string with the image directory
+#	@param textPath a string with the text directory
+#	@returns a profile table as a DataFrame
+def week4(profileTable,liwcTable,relationTable,imagePath,textPath):
+	results = pandas.DataFrame(index=profileTable['userid'])
+	results['age']    = baseline.MEDIAN_AGE
+	results['gender'] = baseline.MEDIAN_GENDER
+	results['ope']    = baseline.MEAN_OPEN
+	results['con']    = baseline.MEAN_CON
+	results['ext']    = baseline.MEAN_EXT
+	results['agr']    = baseline.MEAN_AGR
+	results['neu']    = baseline.MEAN_NEU
+	
+	
+	textResults = textClassifier.classify(profileTable,liwcTable,textPath)
+	results = results.assign(gender=textResults['gender'])
 	
 	return results
 
 
 
 
-
-
 # change this to use a different classifier
-classify = base
+classify = week4
 
--- textClassifier.py
+++ textClassifier.py
@@ -1,8 +1,28 @@
 
+import pandas
+from sklearn import metrics
+from joblib import dump, load
 
-def genderTree(text, liwc):
+TESTING = False
+
+
+def classify(profileTable,liwcTable, textPath):
+	liwcTable.set_index('userid', inplace=True)
+	result = pandas.DataFrame(index=liwcTable.index)
 	
-
-
-classifier = genderTree
+	
+	genderTree = load('text/genderTree.joblib')
+	result['gender'] = genderTree.predict(liwcTable)
+	
+	
+	if TESTING:
+		copy = profileTable.set_index('userid')
+		copy.sort_index(inplace=True)
+		
+		result.sort_index(inplace=True)
+		
+		print(metrics.accuracy_score(copy['gender'],result['gender']))
+	
+	
+	return result
 
