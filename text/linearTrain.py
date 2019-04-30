0b47a49179e7e23de62b80425cdffb25b9e5301d
Ammon Dodson <ammon@ammondodson.info>
2019-04-30 11:58:45 -0700

    rebuilt the linear regression with K-fold

--- text/linearTrain.py
+++ text/linearTrain.py
@@ -7,44 +7,70 @@
 ################################################################################
 
 
+import sys
+
+sys.path.insert(0,'../')
+import dataPreProcess as dpp
+
 import pandas
-from sklearn.linear_model import LinearRegression
-from sklearn import metrics
-from joblib import dump, load
+from sklearn.linear_model    import LinearRegression
+from sklearn.model_selection import KFold
+from sklearn                 import metrics
+from joblib                  import dump
 
 classes = ['age','ope','con','ext','agr','neu']
 
-trainingData = pandas.read_csv("liwcTrain.csv", index_col=0)
-trainingData.drop('userId', axis=1, inplace=True)
+############################## COLLECT SAMPLE DATA #############################
 
-features = list(trainingData.columns[:82])
-X = trainingData[features]
+dataPath = sys.argv[1]
+if not dpp.verify(dataPath):
+	print("could not find data")
+	exit()
+
+trainingData = dpp.combineLIWC(dpp.loadProfile(dataPath), dpp.loadLIWC(dataPath))
+#print(trainingData)
+
+X = trainingData[dpp.LIWC]
 y = trainingData[classes]
 
-#################################### TRAIN #####################################
+#print(X)
+#print(y)
 
-model = LinearRegression().fit(X, y)
+model = LinearRegression()
 
 ##################################### TEST #####################################
 
-testData = pandas.read_csv('liwcTest.csv', index_col=0)
+folds = KFold(10,True)
 
-X = testData[features]
-y = testData[classes]
+for classification in classes:
+	mean = 0.0
+	
+	print(classification)
+	
+	#print(y[classification])
+	
+	for train_index, test_index in folds.split(X):
+		Xtrain = X.iloc[train_index]
+		Xtest  = X.iloc[test_index]
+		yTrain = y[classification].iloc[train_index]
+		yTest  = y[classification].iloc[test_index]
+	
+		model.fit(Xtrain, yTrain)
+	
+		accuracy = metrics.mean_squared_error(yTest,model.predict(Xtest))
+		print(accuracy)
+		mean += accuracy
+	
+	mean /= 10
+	print(classification + " mean: " + str(mean))
+	
 
-p = model.predict(X)
+#################################### TRAIN #####################################
 
-p = pandas.DataFrame(data=p, columns=classes)
-
-print(y['age'])
-print(p['age'])
-
-for i in classes:
-	print(str(i) + ': ' + str(metrics.mean_squared_error(y[i],p[i])))
-
-
-#print(metrics.mean_squared_error(y,p))
+model.fit(X, y)
 
 ################################## SAVE MODEL ##################################
 
 dump(model, "linearRegression.joblib")
+
+
