4f8d6bd16a9293f3c7d9632856b552f78de0ee41
Ammon Dodson, Jake McKenzie, Matt Skipworth <ammon0@uw.edu, jake314@uw.edu, mfskipwo@uw.edu>
2019-06-04 04:36:49 -0700

    update to use utility

--- text/genderTreeTrain.py
+++ text/genderTreeTrain.py
@@ -7,18 +7,34 @@
 ################################################################################
 
 
+import sys
+sys.path.insert(0,'../')
+import utility
+
 import pandas
 from sklearn import tree
 from sklearn import metrics
 from joblib import dump, load
 
+dataPath = sys.argv[1]
+if not utility.verify(dataPath):
+	print('could not find data')
+	exit()
 
-trainingData = pandas.read_csv("liwcTrain.csv", index_col=0)
-trainingData.drop('userId', axis=1, inplace=True)
+trainingData = utility.combineLIWC(
+	utility.loadProfile(dataPath),
+	utility.loadText(dataPath)
+)
 
-features = list(trainingData.columns[:82])
-X      = trainingData[features]
-gender = trainingData["gender"]
+#trainingData = pandas.read_csv("liwcTrain.csv", index_col=0)
+#trainingData.drop('userId', axis=1, inplace=True)
+
+#features = list(trainingData.columns[:82])
+#X      = trainingData[features]
+#gender = trainingData["gender"]
+
+X      = trainingData[utility.LIWC]
+gender = trainingData['gender']
 
 #################################### TRAIN #####################################
 
@@ -32,10 +48,10 @@ dump(model, "genderTree.joblib")
 
 ##################################### TEST #####################################
 
-testData = pandas.read_csv('liwcTest.csv', index_col=0)
+#testData = pandas.read_csv('liwcTest.csv', index_col=0)
 
-X      = trainingData[features]
-gender = trainingData["gender"]
+#X      = trainingData[features]
+#gender = trainingData["gender"]
 
-accuracy = metrics.accuracy_score(gender,model.predict(X))
-print(accuracy)
+#accuracy = metrics.accuracy_score(gender,model.predict(X))
+#print(accuracy)
